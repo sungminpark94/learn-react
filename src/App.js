7f32e048218c5033e43a51c47b30b03be849c791
sungminpark94 <carstoryman94@gmail.com>
2024-09-06 17:32:36 +0900

    react1일차

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 
 
 function App() {
+  // logic
+  console.log("App.js")
+
+  const name = "박성민";
+  const age = 31;
+  
+  const greetingMessage = `안녕하세요. ${name}님 나이가 ${age}세 이시네요.`
+  
+  console.log("🚀 ~ App ~ greetingMessage:", greetingMessage)
+
+  const user = {
+    isLoggedIn: true,
+    role: "admin", 
+  }
+//사용자가 로그인 한 상태이면서, 역할이 'admin'인 경우엔 "관리자 페이지 접근 가능", 아니면 "관리자 페이지 접근 불가능"
+  
+  if(user.isLoggedIn && user.role === 'admin'){
+    console.log('관리자 페이지 접근 가능')
+  } else {
+    console.log('관리자 페이지 접근 불가능!!')
+  }
+
+
+
+
+
+  //view
+
   return (
     <div>
     </div>
